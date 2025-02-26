daeaa93e623890d244f51c9c6bc7df2af37b0aff
Chenyu Wei <101031113+diediebro114514@users.noreply.github.com>
2025-02-25 23:32:20 -0500

    Add files via upload
    SudokuSolver

---
+++ SudokuSolver.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class SudokuSolver {
+    private static final int SIZE = 9;
+    private static final int EMPTY = 0;
+    
+    public static boolean solveSudoku(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (board[row][col] == EMPTY) {
+                    for (int num = 1; num <= SIZE; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solveSudoku(board)) {
+                                return true;
+                            }
+                            board[row][col] = EMPTY;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    private static boolean isValid(int[][] board, int row, int col, int num) {
+        for (int i = 0; i < SIZE; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
+            }
+        }
+        
+        int boxRowStart = (row / 3) * 3;
+        int boxColStart = (col / 3) * 3;
+        
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[boxRowStart + i][boxColStart + j] == num) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    public static void printBoard(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] board = new int[SIZE][SIZE];
+        
+        System.out.println("Enter the Sudoku board row by row, use 0 for empty cells:");
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                board[i][j] = scanner.nextInt();
+            }
+        }
+        scanner.close();
+        
+        if (solveSudoku(board)) {
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists");
+        }
+    }
+}
